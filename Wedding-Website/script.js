cc7076915695119c7ef7c81cdb4ced677766db2b
Richa Srivastava <122597047+richasrivastav@users.noreply.github.com>
2024-03-05 15:23:36 +0530

    Add files via upload

---
+++ Wedding-Website/script.js
@@ -0,0 +1,6 @@
+<button id="myButton">Click me!</button>
+
+const button = document.getElementById("myButton");
+button.addEventListener("click", function() {
+  console.log("Button was clicked!");
+});
